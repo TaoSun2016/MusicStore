6db41feaced58788a3b15e5ae2fb2ba2dc28f8dc
TaoSun2016 <kevinsun9516@gmail.com>
2017-07-19 10:00:35 +1200

    add new actions in store controller

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -9,9 +9,21 @@ namespace MvcMusicStore.Controllers
     public class StoreController : Controller
     {
         // GET: Store
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Hello, from StoreController.Index()";
+        }
+
+        // GET: Store/Browse
+        public string Browse()
+        {
+            return "Hello, from StoreController.Browse()";
+        }
+
+        // GET: Store/Details
+        public string Details()
+        {
+            return "Hello, from StoreController.Details()";
         }
     }
 }
